75e58327e6c0b112714002fede483753c14d8b8e
aimidy <zoe52888@gmail.com>
2025-04-09 21:48:34 +0800

    update: 優化 LineService，增加錯誤處理及媒體檔案下載功能，並改善 webhook 事件處理邏輯

--- src/line/line.service.ts
+++ src/line/line.service.ts
@@ -1,4 +1,5 @@
 import * as line from '@line/bot-sdk';
+import { TextMessage } from '@line/bot-sdk/dist/messaging-api/api';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import * as fs from 'fs';
@@ -6,136 +7,153 @@ import * as path from 'path';
 
 @Injectable()
 export class LineService {
-  readonly channelSecret: string;
-  private readonly channelAccessToken: string;
+    readonly channelSecret: string;
+    private readonly channelAccessToken: string;
 
-  constructor() {
-    const token = process.env.CHANNEL_ACCESS_TOKEN;
-    if (!token) {
-      throw new Error('CHANNEL_ACCESS_TOKEN is not set');
+    constructor() {
+        const token = process.env.CHANNEL_ACCESS_TOKEN;
+        if (!token) {
+            throw new Error('CHANNEL_ACCESS_TOKEN is not set');
+        }
+        this.channelAccessToken = token;
+
+        const channelSecret = process.env.CHANNEL_SECRET;
+        if (!channelSecret) {
+            throw new Error('CHANNEL_SECRET is not set');
+        }
+        this.channelSecret = channelSecret;
     }
-    this.channelAccessToken = token;
 
-    const channelSecret = process.env.CHANNEL_SECRET;
-    if (!channelSecret) {
-      throw new Error('CHANNEL_SECRET is not set');
+    async handleWebhookEvent(event: line.WebhookEvent) {
+        console.log('Received event:', event.type);
+        switch (event.type) {
+            case 'message':
+                await this.handleMessageEvent(event);
+                break;
+            //   case 'follow':
+            //     await this.handleFollowEvent(event);
+            //     break;
+            //   case 'unfollow':
+            //     await this.handleUnfollowEvent(event);
+            //     break;
+            default:
+                console.warn('Unhandled event type:', event.type);
+        }
     }
-    this.channelSecret = channelSecret;
-  }
 
-  async handleWebhookEvent(event: line.WebhookEvent) {
-    console.log('Received event:', event.type);
-    switch (event.type) {
-      case 'message':
-        await this.handleMessageEvent(event);
-        break;
-      //   case 'follow':
-      //     await this.handleFollowEvent(event);
-      //     break;
-      //   case 'unfollow':
-      //     await this.handleUnfollowEvent(event);
-      //     break;
-      default:
-        console.warn('Unhandled event type:', event.type);
+    async handleMessageEvent(event: line.MessageEvent) {
+        const message = event.message;
+
+        console.log('Received message:', message);
+
+        switch (message.type) {
+            case 'text':
+                this.handleTextMessage(event);
+                break;
+            case 'image':
+                this.handleMediaMessage(event);
+                break;
+            case 'video':
+                console.log('Video message:', message.id);
+                break;
+            case 'audio':
+                console.log('Audio message:', message.id);
+                break;
+            case 'location':
+                console.log('Location message:', message.title, message.address);
+                break;
+            case 'sticker':
+                console.log('Sticker message:', message.packageId, message.stickerId);
+                break;
+            default:
+                console.warn('Unhandled message type:', message.type);
+        }
     }
-  }
 
-  async handleMessageEvent(event: line.MessageEvent) {
-    const message = event.message;
+    async handleTextMessage(event: line.MessageEvent) {
+        const replyToken = event.replyToken;
+        const message = event.message;
 
-    console.log('Received message:', message);
+        console.log('Received text message:', message);
 
-    switch (message.type) {
-      case 'text':
-        this.handleTextMessage(event);
-        break;
-      case 'image':
-        this.handleMediaMessage(event);
-        break;
-      case 'video':
-        console.log('Video message:', message.id);
-        break;
-      case 'audio':
-        console.log('Audio message:', message.id);
-        break;
-      case 'location':
-        console.log('Location message:', message.title, message.address);
-        break;
-      case 'sticker':
-        console.log('Sticker message:', message.packageId, message.stickerId);
-        break;
-      default:
-        console.warn('Unhandled message type:', message.type);
+        this.replyMessage(replyToken, [
+            {
+                type: 'text',
+                text: `You said: ${message}`,
+            },
+        ]);
     }
-  }
 
-  async handleTextMessage(event: line.MessageEvent) {
-    const replyToken = event.replyToken;
-    const message = event.message;
+    async handleMediaMessage(event: line.MessageEvent) {
+        const type = event.message.type;
+        const messageId = event.message.id;
 
-    console.log('Received text message:', message);
+        const ext = {
+            image: 'jpg',
+            video: 'mp4',
+            audio: 'm4a',
+        }[type];
 
-    const replyMessage = {
-      type: 'text',
-      text: `You said: ${message}`,
-    };
+        if (!ext) {
+            console.warn('Unsupported media type:', type);
+            return;
+        }
+        const fileName = `${messageId}.${ext}`;
+        const dir = path.join(process.env.WORKER ?? '', 'downloads');
+        const filePath = path.join(dir, fileName);
 
-    const result = await axios.post(
-      'https://api.line.me/v2/bot/message/reply',
-      {
-        replyToken: replyToken,
-        messages: [replyMessage],
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.channelAccessToken}`,
-        },
-      },
-    );
-    console.log('Reply result:', result.data);
-  }
+        fs.mkdirSync(dir, { recursive: true });
 
-  async handleMediaMessage(event: line.MessageEvent) {
-    const type = event.message.type;
-    const messageId = event.message.id;
+        try {
+            const url = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
+            const res = await axios.get(url, {
+                headers: {
+                    Authorization: `Bearer ${this.channelAccessToken}`,
+                },
+                responseType: 'stream',
+            });
 
-    const ext = {
-      image: 'jpg',
-      video: 'mp4',
-      audio: 'm4a',
-    }[type];
+            const writer = fs.createWriteStream(filePath);
+            res.data.pipe(writer);
 
-    if (!ext) {
-      console.warn('Unsupported media type:', type);
-      return;
+            await new Promise<void>((resolve, reject) => {
+                writer.on('finish', resolve);
+                writer.on('error', reject);
+            });
+
+            console.log(`檔案已儲存：${filePath}`);
+            this.replyMessage(event.replyToken, [
+                {
+                    type: 'text',
+                    text: `下載成功\n檔案名稱：${fileName}\n下載路徑：/line/media/${fileName} `,
+                },
+            ]);
+        } catch (error) {
+            console.error('下載失敗:', error);
+        }
     }
-    const fileName = `${messageId}.${ext}`;
-    const dir = path.join(process.env.WORKER ?? '', 'downloads');
-    const filePath = path.join(dir, fileName);
 
-    fs.mkdirSync(dir, { recursive: true });
-
-    try {
-      const url = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
-      const res = await axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${this.channelAccessToken}`,
-        },
-        responseType: 'stream',
-      });
-
-      const writer = fs.createWriteStream(filePath);
-      res.data.pipe(writer);
-
-      await new Promise<void>((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-      });
-
-      console.log(`檔案已儲存：${filePath}`);
-    } catch (error) {
-      console.error('下載失敗:', error);
+    async replyMessage(replyToken: string, messages: line.Message[]) {
+        await axios
+            .post(
+                'https://api.line.me/v2/bot/message/reply',
+                {
+                    replyToken,
+                    messages,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${this.channelAccessToken}`,
+                    },
+                },
+            )
+            .then((res) => {
+                console.log('Reply message response:', res.data);
+            })
+            .catch((error) => {
+                console.error('Error replying message:', error);
+                return null;
+            });
     }
-  }
 }
